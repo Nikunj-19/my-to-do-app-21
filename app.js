40eee3cc0b2aba14ec210a199f99c1749dc9dc62
Nikunj-19 <nikunjpatel79619@gmail.com>
2025-09-19 22:13:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.post("/delete/:index", (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
