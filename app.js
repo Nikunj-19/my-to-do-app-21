0f9cf0a2fdbb64b14bafb97954005eab66090ea8
Nikunj-19 <nikunjpatel79619@gmail.com>
2025-09-19 22:07:54 +0530

    Initial commit: Node.js To-Do app

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+// In-memory tasks
+let tasks = [];
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index", { tasks });
+});
+
+app.post("/add", (req, res) => {
+  const task = req.body.task.trim();
+  if (task) {
+    tasks.push(task);
+  }
+  res.redirect("/");
+});
+
+app.post("/delete/:index", (req, res) => {
+  const index = req.params.index;
+  tasks.splice(index, 1);
+  res.redirect("/");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
